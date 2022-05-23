d6be9cb27c483c83c37c8772b9e95c290209fce4
Ekaterina Sokolova <105356452+SuperPuperKatushka@users.noreply.github.com>
2022-05-23 08:19:05 +0300

    Update db.py

--- parse_selenium_sokolovaIS14/db.py
+++ parse_selenium_sokolovaIS14/db.py
@@ -5,7 +5,7 @@ from object import Phone
 
 class Database:
     def __init__(self):
-        self.con = mysql.connector.connect(host='gendalf.cf', port=3308, user='root', password='1234567890', database='zhelyak')
+        self.con = mysql.connector.connect(host='gendalf.cf', port=3308, user='root', password='1234567890', database='Sokolova')
         self.cur = self.con.cursor()
 
     def upload_phone(self, phone: Phone):
